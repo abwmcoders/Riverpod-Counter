348a47f848e79eec56bfc08b41826ccac4b8a29f
The-made <abwmcode@gmail.com>
2023-04-15 01:01:16 +0100

    ref:completed beginners riverpod implementation

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+final counterStateProvider = StateProvider<int>(
+  (ref) {
+    return 0;
+  },
+);
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var value = ref.watch(counterStateProvider);
+    return Scaffold(
+      body: Center(
+        child: Text("Value: $value",
+            style: Theme.of(context).textTheme.headlineMedium),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            
+            FloatingActionButton(
+              onPressed: () {
+                if(value == 0){
+
+                }
+                else {
+                  ref.read(counterStateProvider.notifier).state--;
+                }
+              },
+              child: const Icon(Icons.remove),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                ref.read(counterStateProvider.notifier).state++;
+              },
+              child: const Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
